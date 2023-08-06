423e8c55319dcc4a973917916d5d1c5e6174678c
Holger Bruchelt <hobru@users.noreply.github.com>
2023-08-06 16:15:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,45 +1,30 @@
-// Create Web server to listen on port 3000
-// This is a basic web server that listens on port 3000 for web requests.
-// It will respond to any request with a JSON object containing the list of comments.
-// This will be the starting point for our application.
-// This is a very basic server, and it is not suitable for production use.
-// Instead, you should use a production-ready web server such as Apache or Nginx.
-// You can also use Node.js to create a production-ready web server.
-// See the following link for more information: http://expressjs.com/en/advanced/best-practice-performance.html
-// Run this file with the following command: node comments.js
+// create web server with node.js
+// run: node comments.js
+// then: http://localhost:8080
+// then: http://localhost:8080/comments
+// then: http://localhost:8080/comments?user=1
+// then: http://localhost:8080/comments?user=1&post=1
 
-// Load the express module.
-var express = require('express');
-// Load the body-parser module.
-var bodyParser = require('body-parser');
-// Create a new express server.
-var app = express();
-// Configure the server to use bodyParser.
-app.use(bodyParser.json());
+var http = require('http');
+var url = require('url');
+var comments = require('./comments');
 
-// Create the list of comments.
-var comments = [
-    { id: 1, author: 'John Doe', text: 'This is one comment' },
-    { id: 2, author: 'Jane Smith', text: 'This is *another* comment' }
-];
+http.createServer(function (req, res) {
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
 
-// Create a route for the server.
-// This route will respond to a GET request at the /api/comments URL.
-// It will send the comments list as a JSON object.
-// This is the route that the client will use to request the list of comments.
-app.get('/api/comments', function(req, res) {
-    // Send the comments list as a JSON object.
-    res.json(comments);
-});
+    res.writeHead(200, {'Content-Type': 'application/json'});
 
-// Create a route for the server.
-// This route will respond to a POST request at the /api/comments URL.
-// It will add a comment to the comments list.
-// This is the route that the client will use to add a new comment.
-app.post('/api/comments', function(req, res) {
-    // Get the next ID.
-    var id = comments.length + 1;
-    // Create a new comment object.
-    var comment = {
-        id: id,
-
+    if (query.user && query.post) {
+        comments.getComments(query.user, query.post, function (err, comments) {
+            if (err) {
+                res.end(JSON.stringify(err));
+            } else {
+                res.end(JSON.stringify(comments));
+            }
+        });
+    } else {
+        res.end(JSON.stringify(comments));
+    }
+}).listen(8080, 'localhost');
+console.log('Server running at http://localhost:8080/');
